43015d1e7d6a2e5a6d3b2e3702df451b4080dd3f
squallml <squallml@126.com>
2016-09-07 22:30:53 +0800

    设置发布按钮

--- weiboSwift/weiboSwift/Classes/Main/MainViewController.swift
+++ weiboSwift/weiboSwift/Classes/Main/MainViewController.swift
@@ -10,8 +10,33 @@ import UIKit
 
 class MainViewController: UITabBarController {
     
+    // MARK:- 懒加载属性
+    private lazy var imageNames = ["tabbar_home","tabbar_message_center","","tabbar_discover","tabbar_profile"]
+    private lazy var composeBtn : UIButton = UIButton()
+    
+    // MARK:- 系统回调函数
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupComposeBtn()
     }
+    
+    
 }
+// MARK:- 设置UI界面
+extension MainViewController{
+    
+    /// 设置发布按钮
+    private func setupComposeBtn(){
+        tabBar.addSubview(composeBtn)
+        composeBtn.setBackgroundImage(UIImage(named: "tabbar_compose_button"), forState: .Normal)
+        composeBtn.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), forState: .Highlighted)
+        composeBtn.setImage(UIImage(named: "tabbar_compose_icon_add"), forState: .Normal)
+        composeBtn.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), forState: .Highlighted)
+        composeBtn.sizeToFit()
+        composeBtn.center = CGPointMake(tabBar.center.x, tabBar.bounds.size.height * 0.5)
+    }
+    
+    
+}
+
